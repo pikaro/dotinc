796826872cec095b172624d450bd3d0d99be3a5f
David Reis <git@d-reis.com>
2025-11-30 00:22:24 +0100

    feat: Add talosctl completion

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,8 +2,10 @@ _SHELL="$(ps -p $$ -o comm=)"
 
 if [[ "${_SHELL}" == *"bash" ]]; then
     SOURCE="${BASH_SOURCE[0]}"
+    export SHELL_NAME="bash"
 elif [[ "${_SHELL}" == *"zsh" ]]; then
     SOURCE="${(%):-%x}"
+    export SHELL_NAME="zsh"
 else
     echo "Unsupported shell: ${_SHELL}. Please use bash or zsh."
 fi
@@ -15,6 +17,23 @@ set -ua
 
 source "${DOTINC}/aliases.sh"
 source "${DOTINC}/common.sh"
+
+_source_dir() {
+    local DIR_PATH=$1
+
+    while IFS= read -r -d '' FILE; do
+        # shellcheck source=/dev/null
+        source "${FILE}"
+    done < <(find "${DIR_PATH}" -type f -name "*.sh" -print0 | sort -z)
+}
+
+if [ -d "${DOTINC}/${SHELL_NAME}.d" ]; then
+    _source_dir "${DOTINC}/${SHELL_NAME}.d"
+fi
+
+if [ -d "${HOME}/.local/dotinc.${SHELL_NAME}.d" ]; then
+    _source_dir "${HOME}/.local/dotinc.${SHELL_NAME}.d"
+fi
 
 set +ua
 
