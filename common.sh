25c3753181c82a3c0252a8fe479da73465b00b2a
David Reis <git@d-reis.com>
2026-03-31 14:20:53 +0200

    feat: Add color

--- common.sh
+++ common.sh
@@ -26,3 +26,27 @@ _debug() {
         _log "[DEBUG] $*"
     fi
 }
+
+_COL_black=30
+_COL_red=31
+_COL_green=32
+_COL_yellow=33
+_COL_blue=34
+_COL_magenta=35
+_COL_cyan=36
+_COL_white=37
+_COL_bright_black=90
+_COL_bright_red=91
+_COL_bright_green=92
+_COL_bright_yellow=93
+_COL_bright_blue=94
+_COL_bright_magenta=95
+_COL_bright_cyan=96
+_COL_bright_white=97
+
+_color() {
+    local COLOR="${1}"
+    local TEXT="${2}"
+    local COL_VAR="_COL_${COLOR// /_}"
+    printf "\e[${!COL_VAR}m%s\e[0m" "${TEXT}"
+}
